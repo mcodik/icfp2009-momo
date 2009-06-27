a93ce316f025d5c3b865e502e7734bd695ab4042
Maurice Codik <mcodik@gmail.com>
2009-06-27 02:40:30 -0700

    can run an iteration

--- src/com/mauricecodik/icfp2009/VirtualMachine.java
+++ src/com/mauricecodik/icfp2009/VirtualMachine.java
@@ -2,12 +2,11 @@ package com.mauricecodik.icfp2009;
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.Arrays;
-import java.util.logging.Logger;
 
 public class VirtualMachine {
-	
-	private static final Logger log = Logger.getLogger("VirtualMachine");
 	
 	public static void main(String[] args) throws Exception {
 		String filename = args[0];
@@ -19,6 +18,7 @@ public class VirtualMachine {
 	}
 	
 	public VirtualMachine() {
+		// 16385
 		input = new double[16385];
 		output = new double[16385];
 		data = new double[16385];
@@ -46,26 +46,31 @@ public class VirtualMachine {
 			int read;
 			while ((read = inputStream.read(buf, 0, 12)) > 0) {
 				
-				StringBuilder sb = new StringBuilder();
-				for (int i = 0; i < read; i++) {
-					sb.append(Integer.toHexString(0x000000FF & buf[i])).append(" ");
-				}
-				
-				log.info("read " + read + " bytes: " + sb.toString());
-				
 				if (frame > data.length) {
 					throw new Exception("reading more than " + data.length + " frames?");
 				}
 				
-				long instruction;
+				int instruction;
 				double datum;
+				
+				ByteBuffer bb = ByteBuffer.wrap(buf);
+				bb.order(ByteOrder.LITTLE_ENDIAN);
+				
 				if (frame % 2 == 0) {
-					datum = readDouble(buf, 0);
-					instruction = readInt(buf, 8, true);
+					datum = bb.getDouble();
+
+//					System.out.println("read double: " + datum);
+					instruction = bb.getInt();
+
+					//System.out.println("read int: " + Integer.toHexString(instruction));
 				}
 				else {
-					instruction = readInt(buf, 0,true);
-					datum = readDouble(buf, 4);
+					instruction = bb.getInt();
+					//System.out.println("read int: " + Integer.toHexString(instruction));
+
+					datum = bb.getDouble();
+
+					//System.out.println("read double: " + datum);
 				}
 				
 				data[frame] = datum;
@@ -80,54 +85,7 @@ public class VirtualMachine {
 			inputStream.close();
 		}
 		
-		log.info("loaded " + frame + " frames from " + filename);
-	}
-	
-	public double readDouble(byte[] buf, int startOffset) {
-		long h = 0x000000FF & ((int)buf[startOffset]);
-		long g = 0x000000FF & ((int)buf[startOffset+1]);
-		long f = 0x000000FF & ((int)buf[startOffset+2]);
-		long e = 0x000000FF & ((int)buf[startOffset+3]);
-		long d = 0x000000FF & ((int)buf[startOffset+4]);
-		long c = 0x000000FF & ((int)buf[startOffset+5]);
-		long b = 0x000000FF & ((int)buf[startOffset+6]);
-		long a = 0x000000FF & ((int)buf[startOffset+7]);
-		
-		long i = (a << 56) | (b << 48) | (c << 40) | (d << 32) 
-		 			| (e << 24) | (f << 16) | (g << 8) | h;
-		
-		double doub = Double.longBitsToDouble(i);
-		log.info("read double: " + Long.toHexString(i) + " " + doub);
-		return doub;
-	}
-
-	public long readInt(byte[] buf, int startOffset) {
-		return readInt(buf, startOffset, false);
-	}
-	
-	public long readInt(byte[] buf, int startOffset, boolean loud) {
-		long d = 0x000000FF & ((int)buf[startOffset]);
-		long c = 0x000000FF & ((int)buf[startOffset+1]);
-		long b = 0x000000FF & ((int)buf[startOffset+2]);
-		long a = 0x000000FF & ((int)buf[startOffset+3]);
-
-		long e = 0xFFFFFFFF & ((a << 24) | (b << 16) | (c << 8) | d);
-		if (loud) {
-			log.info("read int: " + Long.toHexString(e) + " " + e);
-			log.info("   read int: d = " + Long.toHexString(d));
-			log.info("   read int: c = " + Long.toHexString(c));
-			log.info("   read int: b = " + Long.toHexString(b));
-			log.info("   read int: a = " + Long.toHexString(a));
-		}
-		return e;
-	}
-
-	public double getData(int indx) {
-		return data[indx];
-	}
-	
-	public long getProgram(int indx) {
-		return program[indx];
+		System.out.println("loaded " + frame + " frames from " + filename);
 	}
 	
 	public void run(long configuration) {
@@ -137,9 +95,9 @@ public class VirtualMachine {
 
 		input[0x3e80] = configuration;
 		
-		log.info("Running configuration " + configuration);
+		System.out.println("Running configuration " + configuration);
 		
-		for (int iteration = 0; iteration < 1000; iteration++) {
+		for (int iteration = 0; iteration < 10; iteration++) {
 			runIteration();
 		}
 	}
@@ -151,20 +109,20 @@ public class VirtualMachine {
 	public void runIteration() {
 		for (int pc = 0; pc < program.length; pc++) {
 			long instruction = program[pc];
-			//log.info("running: " + Long.toHexString(instruction));
 
-			int opcode = opcode(instruction); 
+			int opcode = opcode(instruction);
 			if (opcode == 0) {
 				
-				int immediate = (int)((instruction >> 14)&2048);
-				int addr = (int)(instruction & 32767);
+				int immediate = (int)((instruction >> 14)&1023);
+				int addr = (int)(instruction & 16383);
+				int sopcode = (int)((instruction >> 24)&15); 
 				
-				runSOp(pc, opcode, immediate, addr);
+				runSOp(pc, sopcode, immediate, addr);
 			}
 			else {
 				
-				int addr1 = (int)(instruction & 32767);
-				int addr2 = (int)((instruction >> 14) & 32767);
+				int addr2 = (int)(instruction & 16383);
+				int addr1 = (int)((instruction >> 14) & 16383);
 				
 				runDOp(pc, opcode, addr1, addr2);
 			}
@@ -172,105 +130,116 @@ public class VirtualMachine {
 	}
 	
 	private void runSOp(int pc, int opcode, int immediate, int addr) {
-		if (opcode == 0) {
-			log.info(Long.toHexString(program[pc]) + ": NOOP");
-			return; 
+		try {
+			if (opcode == 0) {
+				if (program[pc] != 0) {
+					System.out.println(Long.toHexString(program[pc]) + ": NOOP");
+				}
+				return; 
+			}
+			
+			if (opcode == 1) { // CMP
+				int comparator = (int)((immediate>>7)&7);
+				if (comparator == 0) {
+					status = (data[addr] < 0);
+					System.out.println(Long.toHexString(program[pc]) + ": mem[" + addr + "] < 0 == " + status);
+				}
+				else if (comparator == 1) {
+					status = (data[addr] <= 0);
+					System.out.println(Long.toHexString(program[pc]) + ": mem[" + addr + "] <= 0 == " + status);
+				}
+				else if (comparator == 2) {
+					status = (data[addr] == 0);
+					System.out.println(Long.toHexString(program[pc]) + ": mem[" + addr + "] == 0 == " + status);
+				}
+				else if (comparator == 3) {
+					status = (data[addr] >= 0);
+					System.out.println(Long.toHexString(program[pc]) + ": mem[" + addr + "] >= 0 == " + status);
+				}
+				else if (comparator == 1) {
+					status = (data[addr] > 0);
+					System.out.println(Long.toHexString(program[pc]) + ": mem[" + addr + "] > 0 == " + status);
+				}
+				else {
+					throw new RuntimeException("Bad CMP instruction: opcode = " + opcode + "; imm = " + immediate + "; addr = " + addr);
+				}
+				return;
+			}
+			
+			if (opcode == 2) {
+				data[pc] = Math.sqrt( data[addr] );
+				System.out.println(Long.toHexString(program[pc]) + ": mem[" + pc + "] <- sqrt(mem[" + addr + "]) == " + data[pc]);
+				return;
+			}
+			
+			if (opcode == 3) {
+				data[pc] = data[addr];
+				System.out.println(Long.toHexString(program[pc]) + ": mem[" + pc + "] <- mem[" + addr + "] == " + data[pc]);
+				return;
+			}
+			
+			if (opcode == 4) {
+				data[pc] = input[addr];
+				System.out.println(Long.toHexString(program[pc]) + ": mem[" + pc + "] <- input[" + addr + "] == " + data[pc]);
+				return;
+			}
 		}
-		
-		if (opcode == 1) { // CMP
-			if (immediate == 0) {
-				status = (data[addr] < 0);
-				log.info(Long.toHexString(program[pc]) + ": mem[" + addr + "] < 0 == " + status);
-			}
-			else if (immediate == 1) {
-				status = (data[addr] <= 0);
-				log.info(Long.toHexString(program[pc]) + ": mem[" + addr + "] <= 0 == " + status);
-			}
-			else if (immediate == 2) {
-				status = (data[addr] == 0);
-				log.info(Long.toHexString(program[pc]) + ": mem[" + addr + "] == 0 == " + status);
-			}
-			else if (immediate == 3) {
-				status = (data[addr] >= 0);
-				log.info(Long.toHexString(program[pc]) + ": mem[" + addr + "] >= 0 == " + status);
-			}
-			else if (immediate == 1) {
-				status = (data[addr] > 0);
-				log.info(Long.toHexString(program[pc]) + ": mem[" + addr + "] > 0 == " + status);
-			}
-			else {
-				throw new RuntimeException("Bad CMP instruction: opcode = " + opcode + "; imm = " + immediate + "; addr = " + addr);
-			}
-			return;
+		catch (ArrayIndexOutOfBoundsException e) {
+			throw new RuntimeException("Out of bounds while running: opcode = " + opcode + "; imm = " + immediate + "; addr = " + addr, e);
 		}
-		
-		if (opcode == 2) {
-			data[pc] = Math.sqrt( data[addr] );
-			log.info(Long.toHexString(program[pc]) + ": mem[" + pc + "] <- sqrt(mem[" + addr + "]) == " + data[pc]);
-			return;
-		}
-		
-		if (opcode == 3) {
-			data[pc] = data[addr];
-			log.info(Long.toHexString(program[pc]) + ": mem[" + pc + "] <- mem[" + addr + "] == " + data[pc]);
-			return;
-		}
-		
-		if (opcode == 4) {
-			data[pc] = input[addr];
-			log.info(Long.toHexString(program[pc]) + ": mem[" + pc + "] <- input[" + addr + "] == " + data[pc]);
-			return;
-		}
-		
 		throw new RuntimeException("Bad S op: opcode = " + opcode + "; imm = " + immediate + "; addr = " + addr);
 	}
 	
 	private void runDOp(int pc, int opcode, int addr1, int addr2) {
-		
-		if (opcode == 1) {
-			data[pc] = data[addr1] + data[addr2];
-			log.info(Long.toHexString(program[pc]) + ": mem[" + pc + "] <- mem[" + addr1 + "] + mem[" + addr2 + "] == " + data[pc]);
-			return;
-		}
-		
-		if (opcode == 2) {
-			data[pc] = data[addr1] - data[addr2];
-			log.info(Long.toHexString(program[pc]) + ": mem[" + pc + "] <- mem[" + addr1 + "] - mem[" + addr2 + "] == " + data[pc]);
-			return;
-		}
-		
-		if (opcode == 3) {
-			data[pc] = data[addr1] * data[addr2];
-			log.info(Long.toHexString(program[pc]) + ": mem[" + pc + "] <- mem[" + addr1 + "] * mem[" + addr2 + "] == " + data[pc]);
-			return;
-		}
-		
-		if (opcode == 4) {
-			
-			if (data[addr2] == 0.0) {
-				data[pc] = 0.0;
-			}
-			else {
-				data[pc] = data[addr1] / data[addr2];
+		try { 
+			if (opcode == 1) {
+				data[pc] = data[addr1] + data[addr2];
+				System.out.println(Long.toHexString(program[pc]) + ": mem[" + pc + "] <- mem[" + addr1 + "] + mem[" + addr2 + "] == " + data[pc]);
+				return;
 			}
 			
-			log.info(Long.toHexString(program[pc]) + ": mem[" + pc + "] <- mem[" + addr1 + "] / mem[" + addr2 + "] == " + data[pc]);
-			return;
-		}
-		
-		if (opcode == 5) {
-			output[addr1] = data[addr2];
-			log.info(Long.toHexString(program[pc]) + ": output[" + addr1 + "] <- mem[" + addr1 + "] == " + output[addr1]);
-			return;
-		}
-		
-		if (opcode == 6) {
+			if (opcode == 2) {
+				data[pc] = data[addr1] - data[addr2];
+				System.out.println(Long.toHexString(program[pc]) + ": mem[" + pc + "] <- mem[" + addr1 + "] - mem[" + addr2 + "] == " + data[pc]);
+				return;
+			}
 			
-			int src = status ? addr1 : addr2;
-			data[pc] = data[src];
-
-			log.info(Long.toHexString(program[pc]) + ": if " + status + ": data[" + pc + "] <- mem[" + src + "] == " + data[pc]);
-			return;			
+			if (opcode == 3) {
+				data[pc] = data[addr1] * data[addr2];
+				System.out.println(Long.toHexString(program[pc]) + ": mem[" + pc + "] <- mem[" + addr1 + "] * mem[" + addr2 + "] == " + data[pc]);
+				return;
+			}
+			
+			if (opcode == 4) {
+				
+				if (data[addr2] == 0.0) {
+					data[pc] = 0.0;
+				}
+				else {
+					data[pc] = data[addr1] / data[addr2];
+				}
+				
+				System.out.println(Long.toHexString(program[pc]) + ": mem[" + pc + "] <- mem[" + addr1 + "] / mem[" + addr2 + "] == " + data[pc]);
+				return;
+			}
+			
+			if (opcode == 5) {
+				output[addr1] = data[addr2];
+				System.out.println(Long.toHexString(program[pc]) + ": output[" + addr1 + "] <- mem[" + addr1 + "] == " + output[addr1]);
+				return;
+			}
+			
+			if (opcode == 6) {
+				
+				int src = status ? addr1 : addr2;
+				data[pc] = data[src];
+	
+				System.out.println(Long.toHexString(program[pc]) + ": if " + status + ": mem[" + pc + "] <- mem[" + src + "] == " + data[pc]);
+				return;			
+			}
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			throw new RuntimeException("Out of bounds while running: opcode = " + opcode + "; addr1 = " + addr1 + "; addr2 = " + addr2, e);
 		}
 		
 		throw new RuntimeException("Bad D instruction: opcode = " + opcode + "; addr1 = " + addr1 + "; addr2 = " + addr2);
--- tst/com/mauricecodik/icfp2009/VirtualMachineTest.java
+++ tst/com/mauricecodik/icfp2009/VirtualMachineTest.java
@@ -15,18 +15,6 @@ public class VirtualMachineTest {
 		vm = new VirtualMachine();
 	}
 
-	@Test
-	public void testReadInt() {
-		assertEquals(0, vm.readInt(new byte[] { 0, 0, 0, 0 }, 0));
-		assertEquals(1, vm.readInt(new byte[] { 1, 0, 0, 0 }, 0, true));
-		assertEquals(0xFFFFFFFFl, vm.readInt(new byte[] { -1, -1, -1, -1 } , 0));
-		assertEquals(0x00030901l, vm.readInt(new byte[] { 0x01, 0x09, 0x03, 0x00 }, 0));
-	}
-
-	@Test
-	public void testReadDouble() {
-		assertEquals(12.345, vm.readDouble(new byte[] { 0x71, 0x3D, 0x0A, -41, -93, -80, 0x28, 0x40 }, 0));
-	}
 	
 	@Test
 	public void testOpcode() {
@@ -35,17 +23,7 @@ public class VirtualMachineTest {
 		assertEquals(0, vm.opcode(0x01400005));
 	}
 	
-	@Test
-	public void load() throws Exception {
-		vm.load("problems/bin1.obf");
-		
-		assertEquals(0, vm.getProgram(0));
-		System.out.println(Long.toHexString(vm.getProgram(1)));
-		assertEquals(0x00030901l, vm.getProgram(1));
-		
-		assertEquals(0, vm.getData(0));
-		assertEquals(0, vm.getData(1));
-	}
+	
 	
 	
 }
