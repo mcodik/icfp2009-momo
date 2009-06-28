4756b5c1e6911b5c6ab8b3e8fc13003363f18533
Maurice Codik <mcodik@gmail.com>
2009-06-27 22:56:04 -0700

    wait til eta

--- src/com/mauricecodik/icfp2009/vm_runner.clj
+++ src/com/mauricecodik/icfp2009/vm_runner.clj
@@ -32,8 +32,8 @@
      :y (* norm (Math/cos theta)) }))
 
 (defn current-velocity [x y] 
-  { :vx (- x (:sx @prev-data)) 
-    :vy (- y (:sy @prev-data))  })
+  { :vx (Math/abs (- x (:sx @prev-data)))
+    :vy (Math/abs (- y (:sy @prev-data)))  })
 
 (defn apply-deltav [currentv deltav] 
   (let [theta (Math/atan2 (:vx currentv) (:vy currentv))]
@@ -46,7 +46,10 @@
   (* (Math/sqrt (/ mu r1)) (- 1 (Math/sqrt (/ (* 2 r2) (+ r1 r2))))))
 
 (defn hohman-exit-deltav [r1 r2]
-  (* (Math/sqrt (/ mu r1)) (- (Math/sqrt (/ (* 2 r2) (+ r1 r2))) 1)))
+  (* (Math/sqrt (/ mu r2)) (- (Math/sqrt (/ (* 2 r1) (+ r1 r2))) 1)))
+
+(defn hohman-duration [r1 r2]
+  (int (Math/round (* Math/PI (Math/sqrt (/ (Math/pow (+ r1 r2) 3) (* 8 mu)))))))
 
 (defn noop [data] {})
 
@@ -62,27 +65,39 @@
 
 (defn exit-solver [data]
      (if (> 1000.0 (Math/abs (- (:target-radius data) (:current-radius data))))
-       (let [deltav (hohman-entry-deltav (:current-radius data) (:target-radius data))
+       (let [deltav (hohman-exit-deltav (:current-radius data) (:target-radius data))
 	     currentv (current-velocity (:sx data) (:sy data))
 	     newv (apply-deltav currentv deltav)]
 	 (println "hohmann exit: current altitude: " (:current-radius data)  " current v = " currentv 
-		  " computed entry dv = " deltav " newv " newv)
+		  " computed exit dv = " deltav " newv " newv)
 	 (stabilize-next-iter noop)
 	 newv)
        {}))
 
-(defn entry-solver [data]
-     (cond 
-      (= 3 (:t data)) 
-        (let [deltav (hohman-entry-deltav (:current-radius data) (:target-radius data))
-	      currentv (current-velocity (:sx data) (:sy data))
-	      newv (apply-deltav currentv deltav)]
-	  (println "hohmann entry: current altitude: " (:current-radius data) " current v = " currentv 
-		   " computed entry dv = " deltav " newv " newv)
-	  newv)
-      (= 4 (:t data)) (stabilize exit-solver)
-      :otherwise {}))
+(defn stabilize-next-then-wait-until [time next-solver]
+  (dosync (ref-set current-solver
+		   (fn [foo] 
+		       (dosync (ref-set current-solver 
+					(fn [data]
+					    (if (= time (:t data))
+					      (do 
+						(dosync (ref-set current-solver next-solver))
+						(next-solver data))
+					      {}))))
+			       { :vx 0 :vy 0 }))))
 
+(defn entry-solver [data]
+  (if (= 3 (:t data)) 
+    (let [deltav (hohman-entry-deltav (:current-radius data) (:target-radius data))
+	  currentv (current-velocity (:sx data) (:sy data))
+	  newv (apply-deltav currentv deltav)
+	  eta (+ (:t data) (hohman-duration (:current-radius data) (:target-radius data)))]
+      (println "hohmann entry: current altitude: " (:current-radius data) " current v = " currentv 
+	       " computed entry dv = " deltav " newv " newv 
+	       " eta " eta)
+      (stabilize-next-then-wait-until eta exit-solver)
+      newv)
+    {}))
 
 (defn solve-hohman [data] (@current-solver data))
 
